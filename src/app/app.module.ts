ad8ecb074f926f5033ea5435d66c2f04ebc31f39
afmire877 <33981371+afmire877@users.noreply.github.com>
2019-11-19 21:00:40 +0000

    First Setup of boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent,
+    GameComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
