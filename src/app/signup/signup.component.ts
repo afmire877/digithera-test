bf4a94814a4ed87583e94fa035d727bdb6c707c9
afmire877 <33981371+afmire877@users.noreply.github.com>
2019-11-25 20:06:48 +0000

    Added sign up page register function

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { UserService } from '../auth/user.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Navigation, Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -10,18 +11,26 @@ export class SignupComponent implements OnInit {
   @Input() error: string | null;
 
   @Output() submitEM = new EventEmitter();
-  constructor(private user: UserService) {}
+  constructor(private user: UserService, private router: Router) {}
 
   form: FormGroup = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    email: new FormControl('')
+    Username: new FormControl(''),
+    Password: new FormControl(''),
+    Email: new FormControl('')
   });
 
-  submit() {
+  async submit() {
     if (this.form.valid) {
       console.log(this.form.value);
-      this.user.registerUser(this.form.value);
+
+      this.user.registerUser(this.form.value, (err, result) => {
+        if (err) {
+          console.log(err.message);
+          return;
+        }
+        console.log(result);
+      });
+
       this.submitEM.emit(this.form.value);
     }
   }
